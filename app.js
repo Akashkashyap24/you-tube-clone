d9a811262638f98daa4e70172f5c9c1fb74cdc26
Akashkashyap24 <ak7534045938@gmail.com>
2024-07-03 00:47:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector(".manu-icon");
+let sidebar = document.querySelector(".sidebar");
+let container = document.querySelector(".container");
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar")
+    container.classList.toggle("large-container");
+}
